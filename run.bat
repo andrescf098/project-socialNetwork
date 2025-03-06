d03c5256d448c3741a374a15a0f20f75f44f2d4a
Andrés <andrescf@outlook.com>
2025-03-05 19:37:01 -0500

    refactorización en la conexion de la base de datos

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start cmd.exe /k "cd D:\Programacion\project-socialNetwork\backend && npm run dev"
-start cmd.exe /k "cd D:\Programacion\project-socialNetwork\frontend && npm run dev"
+start cmd.exe /k "cd E:\Programacion\project-socialNetwork\backend && npm run dev"
+start cmd.exe /k "cd E:\Programacion\project-socialNetwork\frontend && npm run dev"
